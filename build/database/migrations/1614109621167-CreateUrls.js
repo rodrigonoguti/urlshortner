0b186b95fd2fca1bedaef41e9b347e86d203acf6
Rodrigo Haruo Noguti <rodrigonoguti@gmail.com>
2021-02-23 18:54:12 -0300

    server with ts-node

---
+++ build/database/migrations/1614109621167-CreateUrls.js
@@ -0,0 +1,48 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateUrls1614109621167 = void 0;
+const typeorm_1 = require("typeorm");
+class CreateUrls1614109621167 {
+    up(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield queryRunner.createTable(new typeorm_1.Table({
+                name: "urls",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "originalUrl",
+                        type: "varchar"
+                    },
+                    {
+                        name: "shortUrl",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            }));
+        });
+    }
+    down(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield queryRunner.dropTable("urls");
+        });
+    }
+}
+exports.CreateUrls1614109621167 = CreateUrls1614109621167;
