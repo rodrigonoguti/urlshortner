3efec33b42592b68965a5b7aff83b25196b93e6b
Rodrigo Haruo Noguti <rodrigonoguti@gmail.com>
2021-02-24 09:29:41 -0300

    hiding database credentials

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,21 @@
 import { createConnection } from "typeorm";
 
-createConnection();
+createConnection({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
+  migrations: [
+    "./src/database/migrations/**.ts"
+  ],
+  entities: [
+    "./src/models/**.ts"
+  ],
+  cli: {
+    "migrationsDir": "./src/database/migrations"
+  }
+});
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
+require('dotenv').config();
+
 import "reflect-metadata";
 import express from "express";
 import "./database";
 import { router } from "./routes";
 
 const app = express();
-
-require('dotenv').config();
 
 app.use(express.json());
 app.use(router);
